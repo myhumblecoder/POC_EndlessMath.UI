81d3471bf69a46358139d27395cb31904a26c4a7
Thomas Gooch <ThomasFGooch@Gmail.com>
2023-08-16 15:40:21 -0400

    created blazorwasm, WIP - setting up cors

---
+++ EndlessMath.UI/Program.cs
@@ -0,0 +1,11 @@
+using EndlessMath.UI;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["Apis:Backend"]) });
+
+await builder.Build().RunAsync();
