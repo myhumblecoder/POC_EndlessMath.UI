34ca1a1eaf53c68dfb2c962dd7a3735a5d01bae2
Thomas Gooch <ThomasFGooch@Gmail.com>
2023-08-16 16:09:47 -0400

    able to retrieve data

--- EndlessMath.UI/Program.cs
+++ EndlessMath.UI/Program.cs
@@ -6,6 +6,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["Apis:Backend"]) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["Backend"]) });
 
 await builder.Build().RunAsync();
